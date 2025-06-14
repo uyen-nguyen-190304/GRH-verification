9062b8cfa3d707abbe62f9a83ae6210f4002acae
Uyen Nguyen <nguyen_u1@denison.edu>
2025-06-14 19:07:14 -0400

    feat: add Python bindings for GRH verification with C++ routines

---
+++ grhverify/__init__.py
@@ -0,0 +1,15 @@
+"""
+grhverify: Python bindings for verifying the Generalized Riemann Hypothesis
+using high-performance C++ numerical routines
+
+Exposes:
+    - EULER_CONSTANT
+    - iota(eta)
+    - C(Z)
+    - log_derivative(chi, lam, K)
+    - zero_contribution(gamma_minus, gamma_plus)
+    - rh_verify(d, K, eta, intervals, chi, lam)
+"""
+
+# Import everything from the C++ extension module
+from .grhverify import *
